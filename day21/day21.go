722744025c424cd71e795bb86c22f3d8f456e7f6
Arnold van der Meulen <zwippie@gmail.com>
2022-12-21 23:05:13 +0100

    Some naive searcher that is still running. RPN solver, also work for part A, but then what?

--- day21/day21.go
+++ day21/day21.go
@@ -1,14 +1,17 @@
 package day21
 
-// Directed acyclic graph (DAG)?
+// Directed acyclic graph (DAG)
+// And an RPN equation solver
 
 import (
+	"aoc2022/stack"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
 )
 
-type Operator uint8
+type Operator int
 
 const (
 	None Operator = iota
@@ -17,6 +20,20 @@ const (
 	Multiply
 	Divide
 )
+
+func (o Operator) String() string {
+	switch o {
+	case Add:
+		return "+"
+	case Subtract:
+		return "-"
+	case Multiply:
+		return "*"
+	case Divide:
+		return "/"
+	}
+	return ""
+}
 
 type Node struct {
 	name     string
@@ -36,7 +53,86 @@ func PartA(input []byte) any {
 }
 
 func PartB(input []byte) any {
-	return 0
+	nodes := parseInput(input)
+
+	rpn := &[]any{}
+	toRpn(nodes["root"], rpn)
+	fmt.Printf("rpn: %v\n", rpn)
+	result := evalRpn(rpn)
+	fmt.Printf("result: %v\n", result)
+
+	return 0 // smarterNaiveSearch(nodes)
+}
+
+func evalRpn(rpn *[]any) int {
+	s := stack.Stack[int]{}
+	for _, next := range *rpn {
+		switch val := next.(type) {
+		case int:
+			s.Push(int(val))
+		default:
+			switch next {
+			case Add:
+				s.Push(s.Pop() + s.Pop())
+			case Subtract:
+				b, a := s.Pop(), s.Pop()
+				s.Push(a - b)
+			case Multiply:
+				s.Push(s.Pop() * s.Pop())
+			case Divide:
+				b, a := s.Pop(), s.Pop()
+				s.Push(a / b)
+			}
+		}
+	}
+	return int(s.Pop())
+}
+
+func toRpn(node *Node, result *[]any) {
+	if node.left == nil && node.right == nil {
+		*result = append(*result, node.value)
+	} else {
+		toRpn(node.left, result)
+		toRpn(node.right, result)
+		*result = append(*result, node.operator)
+	}
+}
+
+func smarterNaiveSearch(nodes NodeMap) int {
+	findLeft, findRight := findNode(nodes["root"].left, "humn"), findNode(nodes["root"].right, "humn")
+	fmt.Printf("findLeft: %v\n", findLeft)
+	fmt.Printf("findRight: %v\n", findRight)
+	if findLeft == nil {
+		leftNode := nodes["root"].left
+		leftValue := performOperation(leftNode)
+		newNode := &Node{name: leftNode.name, value: leftValue}
+		nodes[leftNode.name] = newNode
+		nodes["root"].left = newNode
+	} else {
+		rightNode := nodes["root"].right
+		rightValue := performOperation(rightNode)
+		newNode := &Node{name: rightNode.name, value: rightValue}
+		nodes[rightNode.name] = newNode
+		nodes["root"].right = newNode
+	}
+
+	return naiveSearch(nodes)
+}
+
+func naiveSearch(nodes NodeMap) int {
+	nodes["root"].operator = Subtract // should return zero
+	i := 0
+	for {
+		if i%1000000 == 0 {
+			fmt.Printf("i: %v\n", i)
+		}
+		nodes["humn"].value = i
+		if performOperation(nodes["root"]) == 0 {
+			break
+		}
+		i++
+	}
+	return i
 }
 
 func performOperation(node *Node) int {
@@ -51,6 +147,21 @@ func performOperation(node *Node) int {
 		return performOperation(node.left) / performOperation(node.right)
 	}
 	return node.value
+}
+
+func findNode(node *Node, name string) *Node {
+	if node.name == name {
+		return node
+	}
+	if node.left != nil {
+		if found := findNode(node.left, name); found != nil {
+			return found
+		}
+	}
+	if node.right != nil {
+		return findNode(node.right, name)
+	}
+	return nil // not found
 }
 
 func parseInput(input []byte) NodeMap {
